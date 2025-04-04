fc5ccd2c8d4dd6fc0a41de19dfc90f758eb10696
guseppe <yucepi@gmail.com>
2025-04-03 21:27:36 -0400

    leer route paramns

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,12 @@
+import { Navigate, useParams } from "react-router"
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+  const {id, ...rest} = useParams();
+  const hero = getHeroById(id);
+  if(!hero)
+    return <Navigate to='/marvel'/>
   return (
-    <h1>HeroPage</h1>
+    <h1>{hero.superhero}</h1>
   )
 }
