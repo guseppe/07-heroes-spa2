fa19aed342ac9b484b2b8941dddf04fb0b09cb43
guseppe <yucepi@gmail.com>
2025-04-12 17:35:47 -0400

    missing del anterior

--- src/router/PublicRoute.jsx
+++ src/router/PublicRoute.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react"
 import { AuthContext } from "../auth";
 import { Navigate, Outlet } from "react-router";
 
-export const PublicRoute = ({children}) => {
+export const PublicRoute = () => {
   const { logged } = useContext(AuthContext); 
   return (
        (!logged) ? <Outlet/> : <Navigate to='/marvel'  /> 
